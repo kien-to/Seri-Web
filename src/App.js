a398ed16529bff5cc73606889143b46f16d8f416
kien-to <ktto@umass.edu>
2022-06-04 21:15:03 +0700

    Add graphql

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Amplify
-import Amplify from "aws-amplify";
+import {Amplify} from "aws-amplify";
 
 // Pages
 import Home from "./pages/Home"
